157a2f5bc8882f65d7b4bf3bb0d87aa42b5fde96
Nechay Vova <nechayvova2005@gmail.com>
2025-07-05 21:20:56 +0300

    fix: correct button closing tag in App component

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ export const App = () => {
               type="button"
               className="delete ml-3"
               onClick={() => setSelectedGood('')}
-            />
+            ></button>
           </>
         ) : (
           'No goods selected'
